c352bd13db6313c3b4a2d443ed7a803d028a00b0
Florian Albers <xirion@gmx.net>
2022-09-18 19:37:29 +0200

    chore: initial setup

---
+++ packages/saying-hello/example/saying_hello_example.dart
@@ -0,0 +1,6 @@
+import 'package:saying_hello/saying_hello.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/saying-hello/test/saying_hello_test.dart
@@ -0,0 +1,16 @@
+import 'package:saying_hello/saying_hello.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
