d7143b9428cf703148223261c6dc29d881733c5d
Florian Albers <xirion@gmx.net>
2022-09-22 18:23:57 +0200

    feat: implement solution

--- packages/counting-the-number-of-characters/bin/counting_the_number_of_characters.dart
+++ packages/counting-the-number-of-characters/bin/counting_the_number_of_characters.dart
@@ -1,5 +1,5 @@
-import 'package:counting_the_number_of_characters/counting_the_number_of_characters.dart' as counting_the_number_of_characters;
+import 'package:counting_the_number_of_characters/counting_the_number_of_characters.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${counting_the_number_of_characters.calculate()}!');
+void main() {
+  Program.run();
 }
--- packages/counting-the-number-of-characters/lib/counting_the_number_of_characters.dart
+++ packages/counting-the-number-of-characters/lib/counting_the_number_of_characters.dart
@@ -1,3 +1,20 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+import 'package:maybe/maybe.dart';
+
+class Program {
+  static run() {
+    Maybe<String> input = Maybe.nothing();
+
+    do {
+      stdout.write('What is the input string? ');
+      input = stdin.readLineSync().toMaybe();
+
+      input.match(onJust: (value) {
+        print('$value has ${value.length} characters.');
+      }, onNothing: () {
+        print('Please give some input string.');
+      });
+    } while (input.isNothing);
+  }
 }
