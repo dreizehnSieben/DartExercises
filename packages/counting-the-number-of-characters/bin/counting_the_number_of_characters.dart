d41a8d75ad08c3650925ed1ea9e30777e54923b2
Florian Albers <xirion@gmx.net>
2022-09-22 14:14:46 +0200

    chore: initial setup

---
+++ packages/counting-the-number-of-characters/bin/counting_the_number_of_characters.dart
@@ -0,0 +1,5 @@
+import 'package:counting_the_number_of_characters/counting_the_number_of_characters.dart' as counting_the_number_of_characters;
+
+void main(List<String> arguments) {
+  print('Hello world: ${counting_the_number_of_characters.calculate()}!');
+}
---
+++ packages/counting-the-number-of-characters/test/counting_the_number_of_characters_test.dart
@@ -0,0 +1,8 @@
+import 'package:counting_the_number_of_characters/counting_the_number_of_characters.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
