e6316b14190961a16943cfea89f15475601f0000
Florian Albers <xirion@gmx.net>
2022-09-23 08:46:06 +0200

    chore: initial setup

---
+++ packages/printing-quotes/bin/printing_quotes.dart
@@ -0,0 +1,5 @@
+import 'package:printing_quotes/printing_quotes.dart' as printing_quotes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${printing_quotes.calculate()}!');
+}
---
+++ packages/printing-quotes/test/printing_quotes_test.dart
@@ -0,0 +1,8 @@
+import 'package:printing_quotes/printing_quotes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
