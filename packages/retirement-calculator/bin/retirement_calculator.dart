929d61c3bc46c8ce48bfb1c1dfb9e929b7fb221a
Florian Albers <xirion@gmx.net>
2022-10-04 11:37:38 +0200

    feat: implement solution

--- packages/retirement-calculator/bin/retirement_calculator.dart
+++ packages/retirement-calculator/bin/retirement_calculator.dart
@@ -1,5 +1,18 @@
-import 'package:retirement_calculator/retirement_calculator.dart' as retirement_calculator;
+import 'package:user_io/user_io.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${retirement_calculator.calculate()}!');
+void main() {
+  var integerPlease = 'Please give me an integer.';
+
+  var currentAge =
+      userio.readIntegerSync('What is your current age? ', integerPlease);
+
+  var targetAge = userio.readIntegerSync(
+      'At what age would you like to retire? ', integerPlease);
+
+  var ageDifference = targetAge - currentAge;
+  var actualYear = DateTime.now().year;
+  var targetYear = actualYear + ageDifference;
+
+  print('You have $ageDifference years left until you can retire.');
+  print('It\'s $actualYear, so you can retire in $targetYear.');
 }
--- packages/retirement-calculator/test/retirement_calculator_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:retirement_calculator/retirement_calculator.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
