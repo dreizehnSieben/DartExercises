fd8edeb4cfdfbf8c57efdccb229e1fd27b77008f
Florian Albers <xirion@gmx.net>
2022-10-04 11:14:15 +0200

    chore: initial setup

---
+++ packages/retirement-calculator/bin/retirement_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:retirement_calculator/retirement_calculator.dart' as retirement_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${retirement_calculator.calculate()}!');
+}
---
+++ packages/retirement-calculator/test/retirement_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:retirement_calculator/retirement_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
