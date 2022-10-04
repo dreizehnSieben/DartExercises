6e41dbb1f0990a1f747c2b8a85397678a1140135
Florian Albers <xirion@gmx.net>
2022-10-04 09:21:10 +0200

    feat: implement solution

--- packages/simple-math/README.md
+++ packages/simple-math/README.md
@@ -19,7 +19,7 @@ What is the second number? 5
 
 ## Constraints
 
-- Values coming from users will be strings. Ensure that you convert these values to numbers before
-  doing the math.
+- Values coming from users will be strings. Ensure that you convert these values to numbers 
+  before  doing the math.
 - Keep the inputs and outputs separate from the numerical conversions and other processing.
 - Generate a single output statement with line breaks in the appropriate spots.
--- packages/simple-math/bin/simple_math.dart
+++ packages/simple-math/bin/simple_math.dart
@@ -1,5 +1,19 @@
-import 'package:simple_math/simple_math.dart' as simple_math;
+import 'package:user_io/user_io.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${simple_math.calculate()}!');
+void main() {
+  const imperative = 'Please give me a number.';
+
+  var first = userio.readNumberSync('What is the first number? ', imperative);
+
+  var second = userio.readNumberSync('What is the second number? ', imperative);
+
+  var sum = first + second;
+  var diff = first - second;
+  var mult = first * second;
+  var div = first / second;
+
+  print('$first + $second = $sum\n'
+      '$first - $second = $diff\n'
+      '$first * $second = $mult\n'
+      '$first / $second = $div');
 }
